7a55b02a3f6432ae11ca9c4474985ba1ee52b396
Muzammil206 <ismailmuzammil206@gmail.com>
2023-07-07 11:14:29 +0100

    fixed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector('.icon')
 const mobilemenu = document.querySelector('.mobileNav')
+const message = document.querySelectorAll('.hs-accordion-content')
+const btn = document.querySelectorAll('.hs-accordion-toggle')
+
 
 menu.addEventListener('click', function(){
     console.log('ade')
@@ -9,3 +12,35 @@ menu.addEventListener('click', function(){
         mobilemenu.style.display = "block";
     }
 })
+btn.forEach(element => {
+    element.addEventListener('click', function(){
+        console.log('ade')
+       message.forEach((element, nu) => {
+          const content = element
+          console.log(nu)
+          if ( content.style.display === "block") {
+            contentu.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+       });
+    })
+});
+
+
+const typed = new Typed('#text', {
+    strings: [' Photography is poetry & ', ' Beautiful;  '],
+    typeSpeed: 50,
+  });
+const type = new Typed('#cold', {
+    strings: [' The Boolean Egyptian '],
+    cursorChar: '_',
+    typeSpeed: 50,
+});
+
+
+// ScrollReveal().reveal('.revel', { delay: 2000 });
+// ScrollReveal().reveal('.image1', { delay: 500 });
+// ScrollReveal().reveal('.image2', { delay: 1000 });
+// ScrollReveal().reveal('.image3', { delay: 1500 });
+// ScrollReveal().reveal('.image4', { delay: 2000 });
